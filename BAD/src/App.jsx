675b931375d86c33d7ff7247d55b5e54cf9485af
Vikramsaravanan <vikramsavena123@gmail.com>
2026-02-05 11:18:48 +0530

    basic modules and tailwindcss imported

---
+++ BAD/src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
